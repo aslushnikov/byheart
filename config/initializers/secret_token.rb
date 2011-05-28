80c73205a8d2b0a4b263f1bc518de76c8621f7d6
Andrey Lushnikov <aslushnikov@gmail.com>
2011-05-28 12:44:05 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Byheart::Application.config.secret_token = '9101ee5d363310434493218fde440ebc19645cfebe59ffbfb20bc94921c53af4114998ffee88856b766c21d534982bb64173bc793de200359b472cb6edef5f15'
