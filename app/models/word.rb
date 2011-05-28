fca6190baaa3a0cc5ed9f7d6333e2bee6d490569
Andrey Lushnikov <aslushnikov@gmail.com>
2011-05-28 15:11:36 +0400

    добавил дефолтные значения в words

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,3 +1,12 @@
 class Word < ActiveRecord::Base
   belongs_to :user
+  after_initialize :default_values
+
+  protected
+    def default_values 
+      self.orig_show ||= 0
+      self.orig_succ ||= 0
+      self.trans_show ||= 0
+      self.trans_succ ||= 0
+    end
 end
