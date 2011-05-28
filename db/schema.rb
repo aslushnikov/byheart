c01422e9960ab6844caf871387827d2b2c513caa
Andrey Lushnikov <aslushnikov@gmail.com>
2011-05-28 15:03:09 +0400

    добавил связи между объектами

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110528105156) do
+ActiveRecord::Schema.define(:version => 20110528110028) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(:version => 20110528105156) do
     t.integer  "trans_succ"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
 end
