bca1fccef3b047e038cf0d25a6fcd53d80b98bd5
Andrey Lushnikov <aslushnikov@gmail.com>
2011-05-29 12:46:39 +0400

    добавил пронос слов в js

--- app/controllers/web_controller.rb
+++ app/controllers/web_controller.rb
@@ -1,18 +1,22 @@
 class WebController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :load_words
   layout "webclient"
   def home
   end
 
   def train_orig
-    @words = current_user.words
     @train_mode = "orig"
     render :train
   end
 
   def train_trans
-    @words = current_user.words
     @train_mode = "trans"
     render :train
   end
+
+  protected
+    def load_words
+      @words = current_user.words
+    end
 end
