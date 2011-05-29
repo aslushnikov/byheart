b4c3250dda0dce397332eceafc2b47b6b00fa2bb
Alexander Udalov <udalov@acer.(none)>
2011-05-29 12:03:05 +0400

    more table styling, deleting words

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,22 +5,37 @@ function log(s) {
     $('#log').append(s + '\n');
 }
 
+function delete_word_from_table(id) {
+    $('.word' + id).remove();
+    $.ajax({
+        type: 'POST',
+        url: '/api/delete/' + id,
+        success: function(data) {
+        }
+    });
+}
+
 function add_word_to_table(word) {
-    var s = '<td>' + word.id + '</td><td>' + word.orig + '</td><td>' + word.trans + '</td><td>' + word.sample + '</td>';
-    s += '<td><img src="/images/delete.png"><img src="/images/edit_word.png"></td>'
+    var s = '<td>' + word.id + '</td><td>' + word.orig + '</td><td>' + word.trans + '</td><td class="prelastcol">' + word.sample + '</td>';
+    s += '<td class="lastcol"><span class="ctrl' + word.id + '" style="visibility: hidden;">';
+    s += '<span onclick="delete_word_from_table(' + word.id + ')"><img src="/images/delete.png"></span>';
+    s += '&nbsp;&nbsp;&nbsp;';
+    s += '<img src="/images/edit_word.png"></span>';
+    s += '</td>';
     var row = $('<tr></tr>');
     row.html(s);
     row.addClass('word' + word.id);
     row.mouseover(function() {
         var arr = $('.word' + word.id + ' td')
         for (var i = 0; i < arr.length; i++) arr[i].style.backgroundColor = '#ccffcc';
+        arr = $('.ctrl' + word.id);
+        arr[0].style.visibility = 'visible';
     });
     row.mouseout(function() {
         var arr = $('.word' + word.id + ' td')
         for (var i = 0; i < arr.length; i++) arr[i].style.backgroundColor = '#fff';
-    });
-    row.click(function() {
-        log(word.id);
+        arr = $('.ctrl' + word.id);
+        arr[0].style.visibility = 'hidden';
     });
     $('#wt table tbody').append(row);
 }
